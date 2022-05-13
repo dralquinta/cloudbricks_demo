49c3987cc15b19c80a287b432598a074aaa2928d
dralquinta <dralquinta@gmail.com>
2022-05-13 19:34:57 +0000

    Updating License

--- create.sh
+++ create.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+# Copyright (c) 2021 Oracle and/or its affiliates.
+# All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 terraform init
 terraform validate
